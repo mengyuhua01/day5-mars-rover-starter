9748aa223f70a90c8e64a97c586161e73d82e3e2
LEO MENG <mengle@oocl.com>
2025-09-05 15:43:22 +0800

    test: add test should_move_when_executeCommand_given_M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,19 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_move_when_executeCommand_given_M() {
+        //given
+        MarsRover marsRover = new MarsRover(new Location(0,0,Direction.N));
+        //when
+        marsRover.executeCommand(Command.M);
+        Location currentLocation = new Location(0,1,Direction.N);
+        //then
+        assertEquals(currentLocation.getX(),marsRover.getLocation().getX());
+        assertEquals(currentLocation.getY(),marsRover.getLocation().getY());
+        assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
     }
 }
