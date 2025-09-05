8a2bfd0268cfd35e688eea12ce15a4a856a0445b
leo.meng <1336839379@qq.com>
2025-09-06 00:03:23 +0800

    test: add test should_turn_right_90_degrees_when_executeCommand_given_R_Direction_S

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -98,4 +98,14 @@ class DemoTest {
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
+    @Test
+    void should_turn_right_90_degrees_when_executeCommand_given_R_Direction_S() {
+        //given
+        MarsRover marsRover = new MarsRover(new Location(0,0,Direction.S));
+        //when
+        marsRover.executeCommand("R");
+        Location expectedLocation = new Location(0,0,Direction.W);
+        //then
+        assertEquals(expectedLocation, marsRover.getLocation());
+    }
 }
