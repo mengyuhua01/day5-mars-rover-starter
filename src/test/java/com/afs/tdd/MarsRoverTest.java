abcc8076e3b3839f6562e9b2bae3e0b63ee8c7ef
leo.meng <1336839379@qq.com>
2025-09-06 23:30:17 +0800

    refactor: rename DemoTest to MarsRoverTest and update method names for consistency

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -4,166 +4,166 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DemoTest {
+class MarsRoverTest {
     @Test
-    void should_move_when_executeCommand_given_M_Direction_N() {
+    void should_move_when_executeCommands_given_M_Direction_N() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.N));
         //when
-        marsRover.executeCommand("M");
+        marsRover.executeCommands("M");
         Location expectedLocation = new Location(0,1,Direction.N);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
 
     @Test
-    void should_move_when_executeCommand_given_M_Direction_S() {
+    void should_move_when_executeCommands_given_M_Direction_S() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.S));
         //when
-        marsRover.executeCommand("M");
+        marsRover.executeCommands("M");
         Location expectedLocation = new Location(0,-1,Direction.S);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
 
     @Test
-    void should_move_when_executeCommand_given_M_Direction_E() {
+    void should_move_when_executeCommands_given_M_Direction_E() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.E));
         //when
-        marsRover.executeCommand("M");
+        marsRover.executeCommands("M");
         Location expectedLocation = new Location(1,0,Direction.E);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_move_when_executeCommand_given_M_Direction_W() {
+    void should_move_when_executeCommands_given_M_Direction_W() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.W));
         //when
-        marsRover.executeCommand("M");
+        marsRover.executeCommands("M");
         Location expectedLocation = new Location(-1,0,Direction.W);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_left_90_degrees_when_executeCommand_given_L_Direction_N() {
+    void should_turn_left_90_degrees_when_executeCommands_given_L_Direction_N() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.N));
         //when
-        marsRover.executeCommand("L");
+        marsRover.executeCommands("L");
         Location expectedLocation = new Location(0,0,Direction.W);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
 
     @Test
-    void should_turn_left_90_degrees_when_executeCommand_given_L_Direction_S() {
+    void should_turn_left_90_degrees_when_executeCommands_given_L_Direction_S() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.S));
         //when
-        marsRover.executeCommand("L");
+        marsRover.executeCommands("L");
         Location expectedLocation = new Location(0,0,Direction.E);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_left_90_degrees_when_executeCommand_given_L_Direction_E() {
+    void should_turn_left_90_degrees_when_executeCommands_given_L_Direction_E() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.E));
         //when
-        marsRover.executeCommand("L");
+        marsRover.executeCommands("L");
         Location expectedLocation = new Location(0,0,Direction.N);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_left_90_degrees_when_executeCommand_given_L_Direction_W() {
+    void should_turn_left_90_degrees_when_executeCommands_given_L_Direction_W() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.W));
         //when
-        marsRover.executeCommand("L");
+        marsRover.executeCommands("L");
         Location expectedLocation = new Location(0,0,Direction.S);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_right_90_degrees_when_executeCommand_given_R_Direction_N() {
+    void should_turn_right_90_degrees_when_executeCommands_given_R_Direction_N() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.N));
         //when
-        marsRover.executeCommand("R");
+        marsRover.executeCommands("R");
         Location expectedLocation = new Location(0,0,Direction.E);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_right_90_degrees_when_executeCommand_given_R_Direction_S() {
+    void should_turn_right_90_degrees_when_executeCommands_given_R_Direction_S() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.S));
         //when
-        marsRover.executeCommand("R");
+        marsRover.executeCommands("R");
         Location expectedLocation = new Location(0,0,Direction.W);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_right_90_degrees_when_executeCommand_given_R_Direction_E() {
+    void should_turn_right_90_degrees_when_executeCommands_given_R_Direction_E() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.E));
         //when
-        marsRover.executeCommand("R");
+        marsRover.executeCommands("R");
         Location expectedLocation = new Location(0,0,Direction.S);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_turn_right_90_degrees_when_executeCommand_given_R_Direction_W() {
+    void should_turn_right_90_degrees_when_executeCommands_given_R_Direction_W() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.W));
         //when
-        marsRover.executeCommand("R");
+        marsRover.executeCommands("R");
         Location expectedLocation = new Location(0,0,Direction.N);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_move_backward_when_executeCommand_given_B_Direction_N() {
+    void should_move_backward_when_executeCommands_given_B_Direction_N() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.N));
         //when
-        marsRover.executeCommand("B");
+        marsRover.executeCommands("B");
         Location expectedLocation = new Location(0,-1,Direction.N);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_move_backward_when_executeCommand_given_B_Direction_S() {
+    void should_move_backward_when_executeCommands_given_B_Direction_S() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.S));
         //when
-        marsRover.executeCommand("B");
+        marsRover.executeCommands("B");
         Location expectedLocation = new Location(0,1,Direction.S);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_move_backward_when_executeCommand_given_B_Direction_E() {
+    void should_move_backward_when_executeCommands_given_B_Direction_E() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.E));
         //when
-        marsRover.executeCommand("B");
+        marsRover.executeCommands("B");
         Location expectedLocation = new Location(-1,0,Direction.E);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
     }
     @Test
-    void should_move_backward_when_executeCommand_given_B_Direction_W() {
+    void should_move_backward_when_executeCommands_given_B_Direction_W() {
         //given
         MarsRover marsRover = new MarsRover(new Location(0,0,Direction.W));
         //when
-        marsRover.executeCommand("B");
+        marsRover.executeCommands("B");
         Location expectedLocation = new Location(1,0,Direction.W);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
@@ -173,7 +173,7 @@ class DemoTest {
         //given
         MarsRover marsRover = new MarsRover(new Location(0, 0, Direction.N));
         //when
-        marsRover.excuteCommandByBatch("MMLRB");
+        marsRover.executeCommands("MMLRB");
         Location expectedLocation = new Location(0, 1, Direction.N);
         //then
         assertEquals(expectedLocation, marsRover.getLocation());
